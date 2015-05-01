c6b3dae8d5032266f673d738f63cf94596700f4f
SirSavary <jesse.savary@live.com>
2015-04-30 23:50:48 -0400

    Turns out dedupe breaks everything

--- update-production.sh
+++ update-production.sh
@@ -6,10 +6,9 @@ git pull
 # Compile Typescript files into Javascript files
 find . -name "*.ts" | xargs tsc
 
-# Remove packages we no longer need, recursively update our packages, and then deduplicate packages if possible
+# Remove packages we no longer need and then recursively update our packages
 npm prune
 npm --depth 9999 update
-npm dedupe
 
 # Restart the application
 pm2 restart 7
