8eee98e9a3cbbc34cb9f910c36d218110bc193f2
SirSavary <jesse.savary@live.com>
2015-05-19 17:08:13 -0400

    Addon files are not deleted with the addon. Resolves #20

--- api/models/Addon.ts
+++ api/models/Addon.ts
@@ -1,134 +1,157 @@
 /// <reference path='../../typings/node/node.d.ts' />
 
-var Addon = {
-    schema: true,
-    attributes: {
-        name: {
-            type: "string",
-            required: true
-        },
-        price: {
-            type: "float",
-            required: true,
-            min: 0.0,
-            max: 1000.0
-        },
-        gamemode: {
-            type: "string",
-            required: true
-        },
-        category: {
-            type: "string",
-            required: true
-        },
-        zipFile: {
-            type: 'string',
-            required: true
-        },
-        description: {
-            type: 'text'
-        },
-        instructions: {
-            type: 'text'
-        },
-        explanation: {
-            type: 'text',
-            required: true
-        },
-        outsideServers: {
-            type: 'boolean',
-            required: true
-        },
-        containsDrm: {
-            type: 'boolean',
-            required: true
-        },
-        bannerPath: {
-            type: 'string',
-        },
-        galleryImagePaths: {
-            type: 'string',
-        },
-        youtubeVideos: {
-            type: 'string',
-        },
-        autoUpdaterEnabled: {
-            type: 'boolean',
-            defaultsTo: false
-        },
-        configuratorEnabled: {
-            type: 'boolean',
-            defaultsTo: false
-        },
-        leakProtectionEnabled: {
-            type: 'boolean',
-            defaultsTo: false
-        },
-        statTrackerEnabled: {
-            type: 'boolean',
-            defaultsTo: false
+var AddonModel = {
+        schema: true,
+        attributes: {
+            name: {
+                type: "string",
+                required: true
+            },
+            price: {
+                type: "float",
+                required: true,
+                min: 0.0,
+                max: 1000.0
+            },
+            gamemode: {
+                type: "string",
+                required: true
+            },
+            category: {
+                type: "string",
+                required: true
+            },
+            zipFile: {
+                type: 'string',
+                required: true
+            },
+            description: {
+                type: 'text'
+            },
+            instructions: {
+                type: 'text'
+            },
+            explanation: {
+                type: 'text',
+                required: true
+            },
+            outsideServers: {
+                type: 'boolean',
+                required: true
+            },
+            containsDrm: {
+                type: 'boolean',
+                required: true
+            },
+            bannerPath: {
+                type: 'string',
+            },
+            galleryImagePaths: {
+                type: 'string',
+            },
+            youtubeVideos: {
+                type: 'string',
+            },
+            autoUpdaterEnabled: {
+                type: 'boolean',
+                defaultsTo: false
+            },
+            configuratorEnabled: {
+                type: 'boolean',
+                defaultsTo: false
+            },
+            leakProtectionEnabled: {
+                type: 'boolean',
+                defaultsTo: false
+            },
+            statTrackerEnabled: {
+                type: 'boolean',
+                defaultsTo: false
+            },
+
+            discount: {
+                type: "float",
+                defaultsTo: 0.0,
+                max: 0.99,
+                min: 0.0
+            },
+
+            status: {
+                type: "string",
+                enum: ['pending', 'approved', 'denied', 'locked', 'published'],
+                defaultsTo: 'pending'
+            },
+            reasonForUpdate: {
+                type: "string"
+            },
+
+            views: {
+                type: 'string',
+                defaultsTo: 0,
+                min: 0
+            },
+            downloads: {
+                type: 'string',
+                defaultsTo: 0,
+                min: 0
+            },
+
+            // Associations
+            author: {
+                model: 'User'
+            },
+            purchasers: {
+                collection: 'User',
+                via: 'purchases'
+            },
+            transactions: {
+                collection: 'Transaction',
+                via: 'addon'
+            },
+            dependencies: {
+                collection: 'Addon',
+                via: 'dependents'
+            },
+            dependents: {
+                collection: 'Addon',
+                via: 'dependencies'
+            },
+            comments: {
+                collection: 'Comment',
+                via: 'addon'
+            },
+
+            canDownload: function (user) {
+                return !!(this.author == user.id || user.permissionLevel >= 1);
+            },
+
+            canModify: function (user) {
+                return !!(this.author == user.id || user.permissionLevel >= 2);
+            }
         },
 
-        discount: {
-            type: "float",
-            defaultsTo: 0.0,
-            max: 0.99,
-            min: 0.0
-        },
+        beforeDestroy: function (criteria, cb:Function) {
+            Addon.findOne(criteria.where.id)
+                .then(function (addon) {
+                    sails.hooks.gfs.exist({_id: addon.zipFile}, function (err:Error, found:Boolean) {
+                            console.log("Exist results:", err, found);
+                            if (err) {
+                                PrettyError(err, 'An error occurred during sails.hooks.gfs.exist inside Addon.beforeDestroy');
+                                cb(err)
+                            } else if (found) {
+                                sails.hooks.gfs.remove({_id: addon.zipFile}, function (err:Error) {
+                                    if (err) PrettyError(err, 'An error occurred during sails.hooks.gfs.exist inside Addon.beforeDestroy');
+                                    cb()
+                                });
+                            } else {
+                                cb()
+                            }
+                        }
+                    );
+                });
 
-        status: {
-            type: "string",
-            enum: ['pending', 'approved', 'denied', 'locked', 'published'],
-            defaultsTo: 'pending'
-        },
-        reasonForUpdate: {
-            type: "string"
-        },
-
-        views: {
-            type: 'string',
-            defaultsTo: 0,
-            min: 0
-        },
-        downloads: {
-            type: 'string',
-            defaultsTo: 0,
-            min: 0
-        },
-
-        // Associations
-        author: {
-            model: 'User'
-        },
-        purchasers: {
-            collection: 'User',
-            via: 'purchases'
-        },
-        transactions: {
-            collection: 'Transaction',
-            via: 'addon'
-        },
-        dependencies: {
-            collection: 'Addon',
-            via: 'dependents'
-        },
-        dependents: {
-            collection: 'Addon',
-            via: 'dependencies'
-        },
-        comments: {
-            collection: 'Comment',
-            via: 'addon'
-        },
-
-        canDownload: function (user) {
-            return !!(this.author == user.id || user.permissionLevel >= 1);
-        },
-
-        canModify: function (user) {
-            return !!(this.author == user.id || user.permissionLevel >= 2);
         }
     }
-};
+    ;
 
-module.exports = Addon;
+module.exports = AddonModel;
