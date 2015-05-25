2bd557d3b1771613edc477680fb1e337f63a2210
SirSavary <jesse.savary@live.com>
2015-05-25 15:37:16 -0400

    Use Bluebird's Promise.join correctly

--- api/controllers/AddonsController.ts
+++ api/controllers/AddonsController.ts
@@ -16,19 +16,18 @@ module.exports = {
         Promise.join(Addon.count({status: 'published'}), Addon.find({status: 'published'}).paginate({
             page: 0,
             limit: 10
-        }).populateAll())
-            .then(function (totalAddons, addons) {
-                res.view({
-                    title: "Addons",
-                    activeTab: 'addons',
-                    totalAddons: totalAddons[0],
-                    addons: totalAddons[1],
-                    breadcrumbs: true
-                });
-            }).catch(function (err) {
-                PrettyError(err, 'Error occured during Addon.find().paginate() inside AddonsController.index');
-                res.redirect('/');
+        }).populateAll(), function (totalAddons, addons) {
+            res.view({
+                title: "Addons",
+                activeTab: 'addons',
+                totalAddons: totalAddons,
+                addons: totalAddons,
+                breadcrumbs: true
             });
+        }).catch(function (err) {
+            PrettyError(err, 'Error occured during Addon.find().paginate() inside AddonsController.index');
+            res.redirect('/');
+        });
     },
 
     viewAddon: function (req, res) {
@@ -85,14 +84,14 @@ module.exports = {
             });
     },
 
-    purchasePOST: function(req, res) {
+    purchasePOST: function (req, res) {
         console.log('Got purchase POST');
         if (req.user === undefined) {
             req.socket.emit('notification', {type: 'error', msg: 'You must be logged in to make purchases.'})
         } else {
             var addonId:String = req.param('addonId');
             Addon.findOne(addonId).populateAll()
-                .then(function(addon) {
+                .then(function (addon) {
                     if (addon === undefined) return res.send(404);
                     else {
                         var chargeAmount:Number;
@@ -112,7 +111,7 @@ module.exports = {
                             currency: "usd",
                             source: req.param('tokenId'),
                             description: description
-                        }, function(err, charge) {
+                        }, function (err, charge) {
                             if (err && err.type === 'StripeCardError') {
                                 req.socket.emit('notification', {type: 'error', msg: "Your card was declined."})
                             } else {
@@ -123,7 +122,7 @@ module.exports = {
                                     receiverType: 'sale',
                                     addon: addon,
                                     rawData: charge
-                                }).then(function(transaction) {
+                                }).then(function (transaction) {
                                     req.user.sentTransactions.add(transaction);
                                     addon.author.receivedTransactions.add(transaction);
                                     // If the user bought a paid addon we need to track the purchase
@@ -132,9 +131,15 @@ module.exports = {
                                         addon.purchasers.add(req.user);
                                     }
                                     return [req.user.save(), addon.author.save()]
-                                }).spread(function() {
-                                    if (addon.price === 0) req.socket.emit('notification', {type: 'success', msg: "Donation successful!"});
-                                    else req.socket.emit('notification', {type: 'success', msg: "Purchase successful!"});
+                                }).spread(function () {
+                                    if (addon.price === 0) req.socket.emit('notification', {
+                                        type: 'success',
+                                        msg: "Donation successful!"
+                                    });
+                                    else req.socket.emit('notification', {
+                                        type: 'success',
+                                        msg: "Purchase successful!"
+                                    });
                                 });
                             }
                         });
--- api/controllers/StaticController.ts
+++ api/controllers/StaticController.ts
@@ -8,14 +8,19 @@ module.exports = {
         rest: false
     },
 
-    home: function(req, res) {
+    home: function (req, res) {
         Promise.join(
             Tag.find({totalAddons: {'>': 0}}).sort({totalAddons: 'desc'}).limit(3),
-            Addon.find({featured: true,  status: 'published'}).limit(4),
+            Addon.find({featured: true, status: 'published'}).limit(4),
             Addon.find({status: 'published'}).sort({createdAt: 'asc'}).limit(10)
-        )
-        .then(function(array) {
-                res.view({title: 'Mod Mountain', activeTab: 'home', popularTags: array[0], featuredAddons: array[1], latestAddons: array[2]});
+            , function (popularTags, featuredAddons, latestAddons) {
+                res.view({
+                    title: 'Mod Mountain',
+                    activeTab: 'home',
+                    popularTags: popularTags,
+                    featuredAddons: featuredAddons,
+                    latestAddons: latestAddons
+                });
             });
     }
 };
