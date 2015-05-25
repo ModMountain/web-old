6a260ffaa5834ee2d016d97af40b7912f8b5a2b8
SirSavary <jesse.savary@live.com>
2015-05-25 11:00:32 -0400

    Only show published addons on the homepage

--- api/controllers/StaticController.ts
+++ api/controllers/StaticController.ts
@@ -9,7 +9,7 @@ module.exports = {
     },
 
     home: function(req, res) {
-        Promise.join(Tag.find().sort({totalAddons: 'desc'}).limit(3), Addon.find({featured: true}).limit(4), Addon.find().sort({createdAt: 'asc'}).limit(10))
+        Promise.join(Tag.find().sort({totalAddons: 'desc'}).limit(3), Addon.find({featured: true, status: 'published'}).limit(4), Addon.find().sort({createdAt: 'asc', status: 'published'}).limit(10))
         .then(function(array) {
                 res.view({title: 'Mod Mountain', activeTab: 'home', popularTags: array[0], featuredAddons: array[1], latestAddons: array[2]});
             });
