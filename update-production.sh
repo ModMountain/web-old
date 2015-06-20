1cedad7c3582335d6eee3ec106f4dd3fbc47e71d
SirSavary <jesse.savary@live.com>
2015-06-20 16:26:07 -0400

    Make the production update script a bit more logical

--- update-production.sh
+++ update-production.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-# Make sure system dependencies are installed
-apt-get install -y graphicsmagick
-
-# Flush the temporary direct so the Typescript compiler doesn't shit itself
-rm -rf .tmp
-
 # Update our code from Git
 git pull
 
-# Compile Typescript files into Javascript files
-find . -name "*.ts" | xargs tsc --module "commonjs"
+# Make sure system dependencies are installed
+apt-get install -y graphicsmagick
 
 # Remove packages we no longer need and then recursively update our packages
 npm prune
 npm --depth 9999 update
+
+# Flush the temporary direct so the Typescript compiler doesn't shit itself
+rm -rf .tmp
+
+# Compile Typescript files into Javascript files
+find . -name "*.ts" | xargs tsc --module "commonjs"
 
 # Restart the application
 pm2 gracefulReload modmtn
