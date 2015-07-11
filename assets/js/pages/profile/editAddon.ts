437cc4486804eee47af9b510451f72c3aaa2f2ad
SirSavary <jesse.savary@live.com>
2015-07-11 18:16:09 -0400

    Normalize assets, fixes https://github.com/sirsavary/modmountain-issues/issues/14

--- assets/js/pages/profile/editAddon.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='../../../../typings/jquery/jquery.d.ts' />
-/// <reference path='../../../../typings/jquery.validation/jquery.validation.d.ts' />
-
-$(function () {
-    $('select[name^="gamemode"] option[value=' + window.addon.gamemode + ']').attr("selected","selected");
-    $('select[name^="type"] option[value=' + window.addon.type + ']').attr("selected","selected");
-
-    //$('select[name^="type"]').attr("selected","selected");
-});
